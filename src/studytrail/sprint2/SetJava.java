3905f2746b988b4a8d6d42cc97db8a78369a5456
TellesBruno <brunotellesdossantos@gmail.com>
2021-11-05 14:11:39 -0300

    Set heterogeneo e implementação raw

--- src/studytrail/sprint2/SetJava.java
+++ src/studytrail/sprint2/SetJava.java
@@ -5,7 +5,8 @@ import java.util.Set;
 
 public class SetJava {
     public static void main(String[] args) {
-        Set<People> peoples = new HashSet();
+        Set<People> peoples = new HashSet<People>();
+        Set heterogeneous = new HashSet();
         People guest = new People("Ana", "123");
 
         peoples.add(guest);
@@ -16,8 +17,20 @@ public class SetJava {
         peoples.add(null);
         peoples.add(null);
 
+        heterogeneous.add(new People("Maria", "123")); //continuar a parte de implementação raw
+        heterogeneous.add("João");
+        heterogeneous.add(null);
+        heterogeneous.add(14862.0f);
+        heterogeneous.add(7985);
+
         for (People people : peoples) {
             System.out.println(people);
+        }
+        
+        System.out.println("=============================");
+
+        for (Object obj : heterogeneous) {
+            System.out.println(obj);
         }
     }
 }
